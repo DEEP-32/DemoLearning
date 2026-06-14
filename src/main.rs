37771ced8c0b61ea16ad06340f1a5a98f674a055
Deepanshu Singh <anshu3012ep@gmail.com>
2026-06-14 21:27:50 +0530

    [ADD] guess number logic

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-const MAX_POINTS: i32 = 100;
+mod guess_number;
 
 fn main() {
-    let x: i32 = 5;
-    // add comment
-    // one more comment
-    println!("Hello, world! of x is : {}", x);
+    guess_number::guess_number_logic();
 }
