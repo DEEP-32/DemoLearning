0783788811620aa3ce356c6681bcf163ee9bf3b9
Deepanshu Singh <anshu3012ep@gmail.com>
2026-06-21 00:29:56 +0530

    using collection

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,25 @@
-mod enum_learning;
-mod guess_number;
-mod ownership_with_slices;
-mod using_structs;
-use enum_learning::Coin::{self, Quater};
-use using_structs::Rectangle;
-
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
+// mod enum_learning;
+// mod guess_number;
+// mod ownership_with_slices;
+// mod using_structs;
+// use enum_learning::Coin::{self, Quater};
+// use using_structs::Rectangle;
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
+    // let rect1 = Rectangle {
+    //     width: 30,
+    //     height: 50,
+    // };
 
-    println!("The area of the rectangle is {}", rect1.area());
+    // println!("The area of the rectangle is {}", rect1.area());
 
-    let config_max = Some(3u8);
+    // let current_quater = Quater(enum_learning::UsState::Alaska);
 
-    let current_quater = Quater(enum_learning::UsState::Alaska);
+    // if let Coin::Quater(state) = current_quater {
+    //     println!("State quater is from {state:?}!")
+    // } else {
+    // }
 
-    if let Coin::Quater(state) = current_quater {
-        println!("State quater is from {state:?}!")
-    } else {
-    }
+    //using collection
+    let v: Vec<i32> = Vec::new();
 }
