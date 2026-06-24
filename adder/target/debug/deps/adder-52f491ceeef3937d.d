396257a7e060b50597be1f28bc8ab72dea33b1b2
Deepanshu Singh <deepanshus@neelafilm.com>
2026-06-24 18:51:05 +0530

    compiled ne library

---
+++ adder/target/debug/deps/adder-52f491ceeef3937d.d
@@ -0,0 +1,7 @@
+D:\RustProjects\DemoLearning\adder\target\debug\deps\adder-52f491ceeef3937d.d: src\lib.rs
+
+D:\RustProjects\DemoLearning\adder\target\debug\deps\libadder-52f491ceeef3937d.rlib: src\lib.rs
+
+D:\RustProjects\DemoLearning\adder\target\debug\deps\libadder-52f491ceeef3937d.rmeta: src\lib.rs
+
+src\lib.rs:
